2d6c5dbb0bce89d4fcd7f95c08395fcbdde458f1
Caroline Nguyen <caroline.ng.pro@hotmail.com>
2023-11-28 17:10:03 +0100

    repush

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,20 +26,20 @@ musculation_natation = Sport.create!(name: "Musculation")
 
 pierre_sport = UserSport.create!(user_id: pierre.id, sport_id: sport_natation.id)
 
-# training_endurance_natation = Training.create!(
-#   name: "Endurance",
-#   description: "Ma séance d'endurance du lundi du soir pour pour gagner en cardio",
-#   user_sport_id: pierre_sport.id
-# )
+training_endurance_natation = Training.create!(
+  name: "Endurance",
+  description: "Ma séance d'endurance du lundi du soir pour pour gagner en cardio",
+  user_sport_id: pierre_sport.id
+)
 
-# training_sprint_natation = Training.create!(
-#   name: "Sprint",
-#   description: "Ma séance de srint du jundi du soir pour pour gagner en performance",
-#   user_sport_id: pierre_sport.id
-# )
+training_sprint_natation = Training.create!(
+  name: "Sprint",
+  description: "Ma séance de srint du jundi du soir pour pour gagner en performance",
+  user_sport_id: pierre_sport.id
+)
 
-# training_intervalle_natation = Training.create!(
-#   name: "Intervalle",
-#   description: "Ma séance de natation par intervalle pour pour gagner en performance",
-#   user_sport_id: pierre_sport.id
-# )
+training_intervalle_natation = Training.create!(
+  name: "Intervalle",
+  description: "Ma séance de natation par intervalle pour pour gagner en performance",
+  user_sport_id: pierre_sport.id
+)
