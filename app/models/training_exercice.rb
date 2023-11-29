c441f7cb11ec475e14c85031bf8534fe883c3231
Octave Dubois <octave.dubois@hotmail.fr>
2023-11-29 13:15:29 +0100

    avance sur training exercices

--- app/models/training_exercice.rb
+++ app/models/training_exercice.rb
@@ -2,4 +2,10 @@ class TrainingExercice < ApplicationRecord
   belongs_to :training
   belongs_to :exercice
   has_many :training_metrics
+  include PgSearch::Model
+  pg_search_scope :search_exercices,
+  against: [ :name ],
+  using: {
+    tsearch: { prefix: true }
+  }
 end
