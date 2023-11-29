2372f47c07508bcb2aa2ef1ad6133fea338e06fc
Caroline Nguyen <caroline.ng.pro@hotmail.com>
2023-11-29 11:16:49 +0100

    authenfication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!, :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nick_name])
