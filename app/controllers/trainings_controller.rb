9f2fedc932e7ef17b8b43ad6b657ba22ade7d598
Caroline Nguyen <caroline.ng.pro@hotmail.com>
2023-11-29 14:45:28 +0100

    training new/create

--- app/controllers/trainings_controller.rb
+++ app/controllers/trainings_controller.rb
@@ -1,10 +1,27 @@
 class TrainingsController < ApplicationController
   def new
+    @training = Training.new
+    @user_sport = UserSport.find(params[:user_sport_id])
   end
 
   def create
+    @training = Training.new(params_training)
+    @usersport = UserSport.find(params[:user_sport_id])
+    @training.user_sport_id = @usersport.id
+    if @training.save!
+      redirect_to new_training_training_exercice_path(@training)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
   end
+
+  private
+
+  def params_training
+    params.require(:training).permit(:name, :description, :user_sport_id)
+  end
+
 end
