d50c0532f2870fc88a217b1b68cf789cb65b7d29
Caroline Nguyen <caroline.ng.pro@hotmail.com>
2023-12-01 14:32:59 +0100

    seanceedit

--- app/models/training_exercice.rb
+++ app/models/training_exercice.rb
@@ -1,7 +1,8 @@
 class TrainingExercice < ApplicationRecord
   belongs_to :training
   belongs_to :exercice
-  has_many :training_metrics
+  has_many :training_metrics, dependent: :destroy
+  has_many :training_values
   include PgSearch::Model
   pg_search_scope :search_exercices,
   against: [ :name ],
@@ -9,16 +10,17 @@ class TrainingExercice < ApplicationRecord
     tsearch: { prefix: true }
   }
 
-  after_create :create_training_metrics
+  # after_create :create_training_metrics
 
   def self.exercice?(exercice, training)
     find_by(exercice: exercice, training: training).present?
   end
 
-  def create_training_metrics
-    training_metrics = TrainingMetrics.new
-    training_metrics.training_exercices.id = self.id
-  end
+  # def create_training_metrics
+  #   training_metrics = TrainingMetric.new
+  #   training_metrics.training_exercice_id = self.id
+  #   training_metrics.save!
+  # end
 
 
 end
