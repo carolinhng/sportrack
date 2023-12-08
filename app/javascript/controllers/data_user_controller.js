a3221b3cf9c20d98f1e362283a73276c1ecfa2c8
leoMao01 <maony.leo@gmail.com>
2023-12-08 10:50:23 +0100

    finito

--- app/javascript/controllers/data_user_controller.js
+++ app/javascript/controllers/data_user_controller.js
@@ -5,6 +5,7 @@ Chart.defaults.font.family = "Outfit"
 Chart.defaults.font.size = 16;
 Chart.defaults.color = '#262E35';
 
+
 // Connects to data-controller="data-user"
 export default class extends Controller {
   static targets = ["chart"]
@@ -12,22 +13,56 @@ export default class extends Controller {
     seance: Object
   }
 
+
   connect() {
     new Chart(this.chartTarget, {
       type: 'bar',
+
       data: {
         labels: this.seanceValue.x,
         datasets: [{
           label: "Nombre de séances par jour",
           data: this.seanceValue.y,
-          borderWidth: 1
+          borderWidth: 1,
+          borderColor: 'rgb(183, 195, 243)',
+          backgroundColor: 'rgb(183, 195, 243)',
         }]
       },
       options: {
+        plugins: {
+          legend: {
+            display: false,
+          },
+          subtitle: {
+            margin: 1,
+            display: true,
+          }
+        },
         scales: {
           y: {
-            beginAtZero: true
-          }
+            title: {
+              display: false,
+            },
+            ticks: {
+              font: {
+                size: 10,
+              }
+            },
+            grid: {
+              color: 'rgba(245, 245, 246, 1)'
+            }
+          },
+          x: {
+            grid: {
+              display: false,
+            },
+
+            ticks: {
+              font: {
+                size: 10,
+              }
+            }
+          },
         }
       }
     });
